69d5e12ef7fa55f88e62ea1cfc185fa351e60d34
Chanwook Lee <cksdnr1@gmail.com>
2025-04-11 14:13:47 +0900

    [Refactor] magicnumber to variable

--- src/prime-factors.cpp
+++ src/prime-factors.cpp
@@ -5,20 +5,23 @@ class PrimeFactor {
   vector<int> of(int number) {
     vector<int> result = {};
     if (number > 1) {
+      int divisor = 2;
       if (number == 4) {
-        while (number % 2 == 0) {
-          result.push_back(2);
-          number /= 2;
+        while (number % divisor == 0) {
+          result.push_back(divisor);
+          number /= divisor;
         }
       } else if (number == 6) {
-        while (number % 2 == 0) {
-          result.push_back(2);
-          number /= 2;
+        while (number % divisor == 0) {
+          result.push_back(divisor);
+          number /= divisor;
         }
-        while (number % 3 == 0) {
-          result.push_back(3);
-          number /= 3;
+        divisor++;
+        while (number % divisor == 0) {
+          result.push_back(divisor);
+          number /= divisor;
         }
+        divisor++;
       } else {
         result.push_back(number);
       }
