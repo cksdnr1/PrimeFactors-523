5d5d4a2ee103020e88560d57abf3d49470519cea
Chanwook Lee <cksdnr1@gmail.com>
2025-04-11 13:17:20 +0900

    git ignore update

--- build/debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chanwooklee/PJ/CRA/TDD/8.PrimeFactor/vscode
-# Build directory: /Users/chanwooklee/PJ/CRA/TDD/8.PrimeFactor/vscode/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my_test "/Users/chanwooklee/PJ/CRA/TDD/8.PrimeFactor/vscode/build/debug/Test")
-set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/chanwooklee/PJ/CRA/TDD/8.PrimeFactor/vscode/CMakeLists.txt;59;add_test;/Users/chanwooklee/PJ/CRA/TDD/8.PrimeFactor/vscode/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
