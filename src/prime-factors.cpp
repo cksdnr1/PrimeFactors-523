b4fd8447134ac73c13f3b8a79e38c1a022defb1c
Chanwook Lee <cksdnr1@gmail.com>
2025-04-11 14:08:20 +0900

    [Refactor] add while to 2

--- src/prime-factors.cpp
+++ src/prime-factors.cpp
@@ -6,11 +6,7 @@ class PrimeFactor {
     vector<int> result = {};
     if (number > 1) {
       if (number == 4) {
-        if (number % 2 == 0) {
-          result.push_back(2);
-          number /= 2;
-        }
-        if (number % 2 == 0) {
+        while (number % 2 == 0) {
           result.push_back(2);
           number /= 2;
         }
