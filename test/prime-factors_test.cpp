02bb6b8f18f920d258a5235aee895fbd3a6b8394
Chanwook Lee <cksdnr1@gmail.com>
2025-04-11 13:58:07 +0900

    TC Of2 added

--- test/prime-factors_test.cpp
+++ test/prime-factors_test.cpp
@@ -9,6 +9,11 @@ TEST(PrimeFactors, Of1) {
   vector<int> expected = {};
   EXPECT_EQ(expected, prime_factor.of(1));
 }
+TEST(PrimeFactors, Of2) {
+  PrimeFactor prime_factor;
+  vector<int> expected = {2};
+  EXPECT_EQ(expected, prime_factor.of(2));
+}
 
 int main() {
   ::testing::InitGoogleMock();
